b63c86632d6ab32b372ad166bfdf4a39645edd40
Gromov Filipp <gromovfilipp12@mail.ru>
2021-11-08 01:46:57 +0300

    1.dice's changed on empty dice before roll 2.added function for clear results roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,60 +5,62 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.Button
-import android.widget.ImageView
 import java.util.*
-
-
+import android.widget.Button as Button1
+import android.widget.ImageView as ImageView1
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage: ImageView
-    lateinit var diceImage2: ImageView
-    lateinit var diceImage3: ImageView
-    lateinit var diceImage4: ImageView
+    private lateinit var diceImage: ImageView1
+    private lateinit var diceImage2: ImageView1
+    private lateinit var diceImage3: ImageView1
+    private lateinit var diceImage4: ImageView1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //val animationRotateCorner : Animation = AnimationUtils.loadAnimation(
+        //now dont use//val animationRotateCorner : Animation = AnimationUtils.loadAnimation(
            // this, R.anim.rotate_corner)
         diceImage = findViewById(R.id.dice_image)
         diceImage2 = findViewById(R.id.dice_image2)
         diceImage3 = findViewById(R.id.dice_image3)
         diceImage4 = findViewById(R.id.dice_image4)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
+        val rollButton: Button1 = findViewById(R.id.roll_button)
+        val clearButton: Button1 = findViewById(R.id.clear_button)
 
+        clearButton.setOnClickListener {
+            clearDice()
+        }
 
-        val onedice: Button = findViewById(R.id.one_dice)
-        onedice.setOnClickListener{
+        val oneDice: Button1 = findViewById(R.id.one_dice)
+        oneDice.setOnClickListener{
             visOneDice()
             rollButton.setOnClickListener{
                 aniOneDice()
                 rollDice()
             }
         }
-        val twodice: Button = findViewById(R.id.two_dice)
-        twodice.setOnClickListener{
+        val twoDice: Button1 = findViewById(R.id.two_dice)
+        twoDice.setOnClickListener{
             visTwoDice()
             rollButton.setOnClickListener{
                 aniTwoDice()
                 rollDice()
             }
         }
-        val threedice: Button = findViewById(R.id.three_dice)
-        threedice.setOnClickListener{
+        val threeDice: Button1 = findViewById(R.id.three_dice)
+        threeDice.setOnClickListener{
             visThreeDice()
             rollButton.setOnClickListener{
                 aniThreeDice()
                 rollDice()
             }
         }
-        val fourdice: Button = findViewById(R.id.four_dice)
-        fourdice.setOnClickListener{
+        val fourDice: Button1 = findViewById(R.id.four_dice)
+        fourDice.setOnClickListener{
             visFourDice()
             rollButton.setOnClickListener{
                 aniFourDice()
@@ -68,6 +70,7 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+    //visibility dices
     private fun visOneDice(){
         diceImage.visibility = View.VISIBLE
         diceImage2.visibility = View.GONE
@@ -92,6 +95,7 @@ class MainActivity : AppCompatActivity() {
         diceImage3.visibility = View.VISIBLE
         diceImage4.visibility = View.VISIBLE
     }
+    //animation dices
     private fun aniOneDice(){
         val animationRotateCenter : Animation = AnimationUtils.loadAnimation(
             this, R.anim.rotate_center)
@@ -118,6 +122,16 @@ class MainActivity : AppCompatActivity() {
         diceImage3.startAnimation(animationRotateCenter)
         diceImage4.startAnimation(animationRotateCenter)
     }
+    //clear dices
+    private fun clearDice(){
+        val drawableResource = R.drawable.empty_dice
+        diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource)
+        diceImage3.setImageResource(drawableResource)
+        diceImage4.setImageResource(drawableResource)
+
+    }
+    //roll dices
     private fun rollDice() {
 
         val drawableResource = when (Random().nextInt(6) + 1){
